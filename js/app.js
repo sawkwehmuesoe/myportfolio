f2ce4b8b84983fb4d978fab8afce121258d67ff5
Saw Kweh Mue Soe <sawkwehmuesoe@aceplussolutions.com>
2024-06-04 08:58:26 +0700

    about tab section done in pc

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// jQuery Area 
 $(document).ready(function(){
 
     $('.navbuttons').click(function(){
@@ -5,3 +6,25 @@ $(document).ready(function(){
     })
 
 });
+
+// Javascript Area 
+const gettablinks = document.querySelectorAll('.tablink');
+const gettabpages = document.querySelectorAll('.tab-page');
+
+function gettab(evn,linkid){
+
+    gettabpages.forEach(function(gettabpage){
+        gettabpage.style.display = "none";
+
+    });
+
+    for(var x = 0; x < gettablinks.length ; x++){
+        gettablinks[x].classList.remove('active');
+    }
+
+    document.getElementById(linkid).style.display = "block";
+    evn.target.classList.add('active');
+
+}
+
+document.getElementById('autoclick').click();
